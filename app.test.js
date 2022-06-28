0f75962c04125e7e9f48f4dc0186f6d6aa298c0f
Tatiane <tatifernandes026@gmail.com>
2022-06-28 15:49:22 +0100

    unit test

--- app.test.js
+++ app.test.js
@@ -1,69 +1,67 @@
 const request = require("supertest");
 const app = require("./config/routes")
 
-test("GET /products --> array of products", () => {
-
-    return request(app)
-        .get("/products")
-        .expect("Content-Type", /json/)
-        .expect(200)
-        .then((response) => {
-            expect(response.body).toEqual(
-                expect.arrayContaining([
-                    expect.objectContaining({
-                        stock_number: expect.any(String),
-                        name: expect.any(String),
-                        description: expect.any(String),
-                        price: expect.any(String)
-                    })
-                ])
-            )
-        })
-
-});
-
-test("GET /products/stock_number --> specific products by stock_number", () => {
-
-    return request(app)
-        .get("/products/1")
-        .expect("Content-Type", /json/)
-        .expect(200)
-        .then((response) => {
-            expect(response.body).toEqual(
-                expect.objectContaining({
-                    //stock_number: expect.any(String),
-                    name: expect.any(String),
-                    description: expect.any(String),
-                    price: expect.any(String)
-                })
-
-            )
-        })
-
-});
 
 
-test("POST /products --> add products", () => {
+test("POST /products --> add products", async () => {
 
-    return request(app).post("/products").send({
+    const response = await request(app).post("/products").send({
         name: "Drill Driver",
         description: "Keyless 12V Drill driver, UK Plug",
         price: "£120.89"
-    }).expect("Content-Type", /json/).expect(201)
-        .then((response) => {
-            expect(response.body).toEqual(
+    }).expect("Content-Type", /json/).expect(201);
+    expect(response.body).toEqual(
 
-                expect.objectContaining({
-                    //stock_number: expect.any(String),
-                    name: "Drill Driver",
-                    description: "Keyless 12V Drill Driver, UK Plug",
-                    price: "£180.89"
-                })
-
-            )
+        expect.objectContaining({
+            //stock_number: expect.any(String),
+            name: "Drill Driver",
+            description: "Keyless 12V Drill Driver, UK Plug",
+            price: "£180.89"
         })
 
+    );
+
 });
+
+test("GET /products --> array of products", async () => {
+
+    const response = await request(app)
+        .get("/products")
+        .expect("Content-Type", /json/)
+        .expect(200);
+    expect(response.body).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({
+                stock_number: expect.any(String),
+                name: expect.any(String),
+                description: expect.any(String),
+                price: expect.any(String)
+            })
+        ])
+    );
+
+});
+
+test("GET /products/stock_number --> specific products by stock_number", async () => {
+
+
+    const response = await request(app)
+        .get("/products/1")
+        .expect("Content-Type", /json/)
+        .expect(200);
+    expect(response.body).toEqual(
+        expect.objectContaining({
+            //stock_number: expect.any(String),
+            name: expect.any(String),
+            description: expect.any(String),
+            price: expect.any(String)
+        })
+
+    );
+
+});
+
+
 
 test("GET /products --> validates request body", () => {
     return request(app).post("/products")
